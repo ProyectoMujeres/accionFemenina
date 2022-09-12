52e6178cbd0be926add0fcbf6db5e6a632d5eeec
dmarcela2022 <104063341+dmarcela2022@users.noreply.github.com>
2022-09-12 14:12:58 +0200

    #2 modified colors, align items, email changed and adding characters to the telephoneand adding common questions

--- client/src/component/Footer/Footer.jsx
+++ client/src/component/Footer/Footer.jsx
@@ -9,11 +9,12 @@ export default function Footer() {
     <footer className="footer-container">
       <div className="footer-info">
         <h3>Contactos</h3>
-        <p>647956666</p>
-        <p>info@casadelamujer.com</p>
+        <p>Tlf: +34 647 95 66 66</p>
+        <p>info@accionfemenina.com</p>
       </div>
+      <h3>Preguntas frecuentes</h3>
       <div>
-        <IconContext.Provider value={{ size: "3.0em", color: "white",   }}>
+        <IconContext.Provider value={{ size: "3.0em", color: "#f4a460" }}>
           <FaFacebook />
           <AiFillInstagram />
           <AiFillTwitterCircle />
