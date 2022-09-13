6382d04d746d6501ce062dbaa06347cf79a09196
dmarcela2022 <104063341+dmarcela2022@users.noreply.github.com>
2022-09-13 15:48:28 +0200

    #6 created profile structure with style

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,15 @@ import Navbar from "./component/Navbar/Navbar";
 import Footer from './component/Footer/Footer';
 import './App.css';
 import Register from './page/Register/Register';
+import Profile from './page/Profile/Profile';
+
 
 export default function App() {
   return (
     <>
       <Navbar/>
       <Register/>
+      <Profile/>
       <Footer/>
     </>
   );
