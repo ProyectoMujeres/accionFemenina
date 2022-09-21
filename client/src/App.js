2c619447adeafbadd2fe649f83e50735ebaeb75a
julie <j.u.veiga@hotmail.com>
2022-09-21 15:54:52 +0200

    #8 mission and comments structure done, on Home

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,19 @@ import React from 'react';
 import Navbar from "./component/Navbar/Navbar";
 import Footer from './component/Footer/Footer';
 import './App.css';
-import Login from './page/Login/Login';
-import Register from './page/Register/Register';
-import Profile from './page/Profile/Profile';
-import Comment from './page/Comments/Comment';
-import {BrowserRouter} from 'react-router-dom';
+import Home from './page/Home/Home';
+import { BrowserRouter } from 'react-router-dom';
+
 
 export default function App() {
   return (
+    <>
     <BrowserRouter>
-      <Navbar/>    
-        <Login/>
-        <Register/>
-        <Profile/>
-        <Comment/>
+      <Navbar/>
+        <Home/>
       <Footer/>
     </BrowserRouter>
+    </>
   );
 }
 
