a8dfd226b59f9ec68919510076ab9d0962c84da1
Jurki <natsuki.rin111@gmail.com>
2022-09-28 11:39:09 +0200

    #2 create FAQs page, add style, #2 #3 create the routes with links located in footer and navbar and modif style of menu

--- client/src/router/Router.jsx
+++ client/src/router/Router.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Comment from "../page/Comments/Comment";
+import FAQs from "../page/FAQs/FAQs";
 import Home from "../page/Home/Home";
 import Login from "../page/Login/Login";
 import Profile from "../page/Profile/Profile";
@@ -17,6 +18,7 @@ export default function Router() {
                     <Route exact path='/mi-perfil' element={<Profile />}/>
                 </Route> 
                 <Route exact path='/publicaciones' element={<Comment />}/>
+                <Route exact path='/preguntas-frecuentes' element={<FAQs />}/>
             </Routes>
         </>
   )
